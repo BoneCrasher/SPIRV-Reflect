32fffd1889d9e4f258c085db01035ec1b9e5f632
BoneCrasher <boehm.marc.anton@gmail.com>
2019-02-20 16:24:11 +0100

    Update main_descriptors.cpp
    Code cleanup to make it readable.

--- examples/main_descriptors.cpp
+++ examples/main_descriptors.cpp
@@ -1,73 +1,95 @@
+#include <cassert>
 #include "common.h"
 #include "sample_spv.h"
-#include <cassert>
 
 #if defined(SPIRV_REFLECT_HAS_VULKAN_H)
+
 #include <vulkan/vulkan.h>
-struct DescriptorSetLayoutData {
-  uint32_t set_number;
-  VkDescriptorSetLayoutCreateInfo create_info;
+struct DescriptorSetLayoutData 
+{
+  uint32_t                                  set_number;
+  VkDescriptorSetLayoutCreateInfo           create_info;
   std::vector<VkDescriptorSetLayoutBinding> bindings;
 };
-#endif
+
+#endif // defined(SPIRV_REFLECT_HAS_VULKAN_H)
 
 int main(int argn, char** argv)
 {
   SpvReflectShaderModule module = {};
-  SpvReflectResult result = spvReflectCreateShaderModule(sizeof(k_sample_spv), k_sample_spv, &module);
-  assert(result == SPV_REFLECT_RESULT_SUCCESS);
-
+  SpvReflectResult       result = SPV_REFLECT_RESULT_SUCCESS;
+  
+  result = spvReflectCreateShaderModule(sizeof(k_sample_spv), k_sample_spv, &module);
+  assert(SPV_REFLECT_RESULT_SUCCESS == result);
+  
   uint32_t count = 0;
-  result = spvReflectEnumerateDescriptorSets(&module, &count, NULL);
-  assert(result == SPV_REFLECT_RESULT_SUCCESS);
+  std::vector<SpvReflectDescriptorSet*> sets;  
 
-  std::vector<SpvReflectDescriptorSet*> sets(count);
+  result = spvReflectEnumerateDescriptorSets(&module, &count, nullptr);
+  assert(SPV_REFLECT_RESULT_SUCCESS == result);
+  
+  sets.resize(count);
+  
   result = spvReflectEnumerateDescriptorSets(&module, &count, sets.data());
-  assert(result == SPV_REFLECT_RESULT_SUCCESS);
+  assert(SPV_REFLECT_RESULT_SUCCESS == result);
 
 #if defined(SPIRV_REFLECT_HAS_VULKAN_H)
+  
   // Demonstrates how to generate all necessary data structures to create a
   // VkDescriptorSetLayout for each descriptor set in this shader.
   std::vector<DescriptorSetLayoutData> set_layouts(sets.size(), DescriptorSetLayoutData{});
-  for (size_t i_set = 0; i_set < sets.size(); ++i_set) {
+  
+  for (size_t i_set = 0; i_set < sets.size(); ++i_set)
+  {
     const SpvReflectDescriptorSet& refl_set = *(sets[i_set]);
+    
     DescriptorSetLayoutData& layout = set_layouts[i_set];
     layout.bindings.resize(refl_set.binding_count);
-    for (uint32_t i_binding = 0; i_binding < refl_set.binding_count; ++i_binding) {
-      const SpvReflectDescriptorBinding& refl_binding = *(refl_set.bindings[i_binding]);
-      VkDescriptorSetLayoutBinding& layout_binding = layout.bindings[i_binding];
-      layout_binding.binding = refl_binding.binding;
-      layout_binding.descriptorType = static_cast<VkDescriptorType>(refl_binding.descriptor_type);
+    
+    for (uint32_t i_binding = 0; i_binding < refl_set.binding_count; ++i_binding)
+    {
+      SpvReflectDescriptorBinding const &refl_binding = *(refl_set.bindings[i_binding]);
+      
+      VkDescriptorSetLayoutBinding &layout_binding = layout.bindings[i_binding];
+      layout_binding.binding         = refl_binding.binding;
+      layout_binding.descriptorType  = static_cast<VkDescriptorType>(refl_binding.descriptor_type);
       layout_binding.descriptorCount = 1;
-      for (uint32_t i_dim = 0; i_dim < refl_binding.array.dims_count; ++i_dim) {
+      
+      for (uint32_t i_dim = 0; i_dim < refl_binding.array.dims_count; ++i_dim) 
+      {
         layout_binding.descriptorCount *= refl_binding.array.dims[i_dim];
       }
+      
       layout_binding.stageFlags = static_cast<VkShaderStageFlagBits>(module.shader_stage);
     }
-    layout.set_number = refl_set.set;
-    layout.create_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    
+    layout.set_number               = refl_set.set;
+    layout.create_info.sType        = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
     layout.create_info.bindingCount = refl_set.binding_count;
-    layout.create_info.pBindings = layout.bindings.data();
+    layout.create_info.pBindings    = layout.bindings.data();
   }
+  
   // Nothing further is done with set_layouts in this sample; in a real application
   // they would be merged with similar structures from other shader stages and/or pipelines
   // to create a VkPipelineLayout.
+  
 #endif
 
   // Log the descriptor set contents to stdout
-  const char* t  = "  ";
-  const char* tt = "    ";
+  char const* t  = "  ";
+  char const* tt = "    ";
 
   PrintModuleInfo(std::cout, module);
   std::cout << "\n\n";
 
   std::cout << "Descriptor sets:" << "\n";
-  for (size_t index = 0; index < sets.size(); ++index) {
+  for (size_t index = 0; index < sets.size(); ++index) 
+  {
     auto p_set = sets[index];
 
     // descriptor sets can also be retrieved directly from the module, by set index
     auto p_set2 = spvReflectGetDescriptorSet(&module, p_set->set, &result);
-    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    assert(SPV_REFLECT_RESULT_SUCCESS == result);
     assert(p_set == p_set2);
     (void)p_set2;
 
